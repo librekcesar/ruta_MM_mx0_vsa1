7f66c7cc2c1dfe359ac799acad9c3f4dbd2a6b4e
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 17:19:34 -0600

    Add files via upload

---
+++ estaciones_mx0_vsa1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_vsa1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "C. Miguel Hidalgo y Costilla 245, Tamulte de las Barrancas, 86150 Villahermosa, Tab.", "description": "VSA1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -92.956401, 17.9701173, 0.0 ] } }
+]
+}
---
+++ paradas_mx0_vsa1_copia.js
@@ -0,0 +1,11 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mx0_vsa1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": null, "layer": "PEMEX_MX0_VSA", "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -95.536127, 18.155614, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estación de Servicio Pemex", "description": null, "layer": "PEMEX_MX0_VSA", "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -97.079513, 18.838492, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex SG Gasolineros", "description": null, "layer": "PEMEX_MX0_VSA", "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -98.277677, 19.140514, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": null, "layer": "PEMEX_MX0_VSA", "direccion": null }, "geometry": { "type": "Point", "coordinates": [ -96.280935, 18.651426, 0.0 ] } }
+]
+}
